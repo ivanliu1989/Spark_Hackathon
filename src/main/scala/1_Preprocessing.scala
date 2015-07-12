6331f7998026a7ef0acbd0b4c3f4f401fbaf733f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-12 22:08:34 +1000

    read data

--- src/main/scala/1_Preprocessing.scala
+++ src/main/scala/1_Preprocessing.scala
@@ -11,12 +11,12 @@ object Preprocessing {
     val conf = new SparkConf().setAppName("Streaming Example").setMaster("local[2]")
     val sc = new SparkContext(conf)
 
-    val offers = sc.textFile("../data/offers")
+    val offers = sc.textFile("../data/offers").map(_.split(","))
     val testHist = sc.textFile("../data/testHistory")
     val trainHist = sc.textFile("../data/trainHistory")
     val transactions = sc.textFile("../data/transactions")
-    print(transactions.count())
-
+//    print(transactions.count())
+    offers.foreach(println)
   }
 }
 
