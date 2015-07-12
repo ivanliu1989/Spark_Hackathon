aece7f6e75fc51c37a983285f574a666a94a66be
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-12 21:49:30 +1000

    read data

---
+++ src/main/scala/1_Preprocessing.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * Created by ivanliu on 12/07/15.
+ */
+
+object Preprocessing {
+
+  def main(args: Array[String]): Unit = {
+    //    define Spark Context
+    val conf = new SparkConf().setAppName("Streaming Example").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    val offers = sc.textFile("../data/offers")
+    val testHist = sc.textFile("../data/testHistory")
+    val trainHist = sc.textFile("../data/trainHistory")
+    val transactions = sc.textFile("../data/transactions")
+    print(transactions.count())
+
+  }
+}
+
