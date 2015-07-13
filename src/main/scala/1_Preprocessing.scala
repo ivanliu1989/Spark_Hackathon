f429035993be7850628b5b3bfe91cb334b21f915
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-13 23:07:28 +1000

    read data

--- src/main/scala/1_Preprocessing.scala
+++ src/main/scala/1_Preprocessing.scala
@@ -10,13 +10,28 @@ object Preprocessing {
     //    define Spark Context
     val conf = new SparkConf().setAppName("Streaming Example").setMaster("local[2]")
     val sc = new SparkContext(conf)
-
+    //Load and Check the data
     val offers = sc.textFile("../data/offers").map(_.split(","))
     val testHist = sc.textFile("../data/testHistory")
     val trainHist = sc.textFile("../data/trainHistory")
     val transactions = sc.textFile("../data/transactions")
-//    print(transactions.count())
-    offers.foreach(println)
+    val tran_sample = transactions.sample(false, fraction = 0.00001, seed = 123)
+    print(tran_sample.count())
+    //    tran_sample.foreach(println)
+  }
+
+  def parsePoint(line:String): Unit ={
+    /*Converts a comma separated unicode string into a `LabeledPoint`.
+
+    Args:
+        line (unicode): Comma separated unicode string where the first element is the label and the
+            remaining elements are features.
+
+    Returns:
+        LabeledPoint: The line is converted into a `LabeledPoint`, which consists of a label and
+            features.*/
+    
+
   }
 }
 
