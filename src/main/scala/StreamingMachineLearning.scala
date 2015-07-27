fea18b5bd38072796abcbd08720fd7ec59178eb1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-27 23:36:46 +1000

    reshape

--- src/main/scala/StreamingMachineLearning.scala
+++ src/main/scala/StreamingMachineLearning.scala
@@ -1,67 +1,57 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
-
 
 /**
  * @author ivanliu
  */
 object StreamingMachineLearning {
-  
+
   //val date_format = new java.text.SimpleDateFormat("yyyy-MM-dd")
 
   def main(args: Array[String]) {
-    
+
     // Define Spark Context
     val sparkConf = new SparkConf().setAppName("RDDRelation").setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sc)
-    
+
     // Importing the SQL context gives access to all the SQL functions and implicit conversions.
     import sqlContext.implicits._
-    
+
     //Load and Check the data
     val offers_df = sc.textFile("../data/offers")
     val testHist_df = sc.textFile("../data/testHistory")
     val trainHist_df = sc.textFile("../data/trainHistory")
     val transactions_df = sc.textFile("../data/transactions")
-    
+
     // SQL Table offers
     case class offers(offer: String, category: String, quantity: Int, Company: String, offervalue: Double, brand: String)
-    val offers_data = offers_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
-                               .map(_.split(","))
-                               .map(r=>offers(r(0),r(1),r(2).toInt,r(3),r(4).toDouble,r(5))).toDF()
-                               
-    offers_data.registerTempTable("offers_table") 
-    
+    val offers_data = offers_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(",")).map(r => offers(r(0), r(1), r(2).toInt, r(3), r(4).toDouble, r(5))).toDF()
+
+    offers_data.registerTempTable("offers_table")
+
     // SQL Table testHist
     case class testHist(id: String, chain: Int, offer: String, market: String, offerdate: String)
-    val testHist_data = testHist_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
-                                   .map(_.split(","))
-                                   .map(r=>testHist(r(0),r(1).toInt,r(2),r(3),r(4))).toDF()
-                               
-    testHist_data.registerTempTable("testHist_table") 
-    
+    val testHist_data = testHist_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(",")).map(r => testHist(r(0), r(1).toInt, r(2), r(3), r(4))).toDF()
+
+    testHist_data.registerTempTable("testHist_table")
+
     // SQL Table trainHist
     case class trainHist(id: String, chain: Int, offer: String, market: String, repeattrips: Int, repeater: String, offerdate: String)
-    val trainHist_data = trainHist_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
-                                   .map(_.split(","))
-                                   .map(r=>trainHist(r(0),r(1).toInt,r(2),r(3),r(4).toInt,r(5),r(6))).toDF()
-                               
-    trainHist_data.registerTempTable("trainHist_table") 
-    
+    val trainHist_data = trainHist_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(",")).map(r => trainHist(r(0), r(1).toInt, r(2), r(3), r(4).toInt, r(5), r(6))).toDF()
+
+    trainHist_data.registerTempTable("trainHist_table")
+
     // SQL Table transactions
-    case class transactions(id: String, chain: Int, dept: String, category: String, company: String, brand: String, date: String, 
-                            productsize: String, productmeasure: String, purchasequantity: Int, purchaseamount: Double)
-    val transactions_data = transactions_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
-                                           .map(_.split(","))
-                                           .map(r=>transactions(r(0),r(1).toInt,r(2),r(3),r(4),r(5),r(6),r(6),r(7),r(8),r(9).toInt,r(10).toDouble)).toDF()
-                               
-    transactions_data.registerTempTable("transactions_table") 
-    
+//    case class transactions(id: String, chain: Int, dept: String, category: String, company: String, brand: String, date: String, productsize: String, productmeasure: String, purchasequantity: Int, purchaseamount: Double)
+//    val transactions_data = transactions_df.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(",")).map(r => transactions(r(0), r(1).toInt, r(2), r(3), r(4), r(5), r(6), r(6), r(7), r(8), r(9).toInt, r(10).toDouble)).toDF()
+//
+//    transactions_data.registerTempTable("transactions_table")
+
     // SQL Query
-    sqlContext.sql("Select * from offers_table").show
-    
+    sqlContext.sql("select offerdate from offers_table").collect().foreach(println)
+
   }
-  
+
 }
