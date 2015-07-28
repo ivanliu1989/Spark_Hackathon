a6cbc96c91f1e1e3fd1a43a53def5b6a034f64b3
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-28 15:04:54 +1000

    reduce data

--- src/main/scala/StreamingMachineLearning.scala
+++ src/main/scala/StreamingMachineLearning.scala
@@ -20,27 +20,27 @@ object StreamingMachineLearning {
 
     //Load and Check the data
     val offers_df = sc.textFile("../data/offers").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(","))
-    val testHist_df = sc.textFile("../data/testHistory").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(",")).
+    val testHist_df = sc.textFile("../data/testHistory").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(","))
     val trainHist_df = sc.textFile("../data/trainHistory").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(","))
     val transactions_df = sc.textFile("../data/transactions").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.map(_.split(","))
 
     // SQL Table offers
-    case class offers(offer: String, category: String, quantity: Int, Company: String, offervalue: Double, brand: String)
-    val offers_data = offers_df.map(r => offers(r(0), r(1), r(2).toInt, r(3), r(4).toDouble, r(5))).toDF()
-    
-    offers_data.registerTempTable("offers_table")
+//    case class offers(offer: String, category: String, quantity: Int, Company: String, offervalue: Double, brand: String)
+//    val offers_data = offers_df.map(r => offers(r(0), r(1), r(2).toInt, r(3), r(4).toDouble, r(5))).toDF()
+//    
+//    offers_data.registerTempTable("offers_table")
 
     // SQL Table testHist
-    case class testHist(id: String, chain: Int, offer: String, market: String, offerdate: String)
-    val testHist_data = testHist_dfmap(r => testHist(r(0), r(1).toInt, r(2), r(3), r(4))).toDF()
-
-    testHist_data.registerTempTable("testHist_table")
+//    case class testHist(id: String, chain: Int, offer: String, market: String, offerdate: String)
+//    val testHist_data = testHist_dfmap(r => testHist(r(0), r(1).toInt, r(2), r(3), r(4))).toDF()
+//
+//    testHist_data.registerTempTable("testHist_table")
 
     // SQL Table trainHist
-    case class trainHist(id: String, chain: Int, offer: String, market: String, repeattrips: Int, repeater: String, offerdate: String)
-    val trainHist_data = trainHist_df.map(r => trainHist(r(0), r(1).toInt, r(2), r(3), r(4).toInt, r(5), r(6))).toDF()
-
-    trainHist_data.registerTempTable("trainHist_table")
+//    case class trainHist(id: String, chain: Int, offer: String, market: String, repeattrips: Int, repeater: String, offerdate: String)
+//    val trainHist_data = trainHist_df.map(r => trainHist(r(0), r(1).toInt, r(2), r(3), r(4).toInt, r(5), r(6))).toDF()
+//
+//    trainHist_data.registerTempTable("trainHist_table")
 
     // SQL Table transactions
 //    case class transactions(id: String, chain: Int, dept: String, category: String, company: String, brand: String, date: String, productsize: String, productmeasure: String, purchasequantity: Int, purchaseamount: Double)
@@ -49,14 +49,14 @@ object StreamingMachineLearning {
 //    transactions_data.registerTempTable("transactions_table")
 
     // SQL Query
-    sqlContext.sql("select offerdate from offers_table").collect().foreach(println)
+    // sqlContext.sql("select offerdate from offers_table").collect().foreach(println)
     
     // Get all categories and comps on offer in a dict
-    val offer_cat = offers_df.map(r => r(1)).distinct().collect()
-    val offer_comp = offers_df.map(r => r(3)).distinct().collect()
+    val offer_cat = offers_df.map(r => r(1)).collect()
+    val offer_comp = offers_df.map(r => r(3)).collect()
     
     // only write when if category in offers dict
-    val transactions_data_filtered = trainHist_df.filter(r=>{offer_cat.contains(r(1)) or offer_comp.contains(r(3))})
+    val transactions_df_filtered =  transactions_df.filter(r=>{offer_cat.contains(r(1)) || offer_comp.contains(r(3))})
   }
 
 }
