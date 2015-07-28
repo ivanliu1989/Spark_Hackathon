e44738f51c74858ac65d99069df0e9c5199dc600
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-28 16:43:11 +1000

    clean-up step 1

--- src/main/scala/utilClasses/offers.scala
+++
@@ -1,8 +0,0 @@
-package utilClasses
-
-/**
- * @author ivanliu
- */
-class offers(offer: Int, category: Int, quantity: Int, Company: Int, offervalue: Double, brand: Int) {
-  
-}
--- src/main/scala/utilClasses/trainHist.scala
+++
@@ -1,8 +0,0 @@
-package utilClasses
-
-/**
- * @author ivanliu
- */
-class trainHist {
-  
-}
