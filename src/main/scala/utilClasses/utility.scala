833ba0af3dc675869ffad3baa3672ea6e76a2f01
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-07-28 12:09:11 +1000

    date diff

--- src/main/scala/utilClasses/utility.scala
+++ src/main/scala/utilClasses/utility.scala
@@ -6,12 +6,13 @@ package utilClasses
 class utility {
   
   val date_format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+  val date_unit = 1.15741e-8
   
-  def diff_days(s1: String, s2: String, TimeUnit timeUnit) = {
+  def diff_days(s1: String, s2: String) = {
     val date1 = date_format.parse(s1)
     val date2 = date_format.parse(s2)
     val delta: Long = date2.getTime() - date1.getTime()
-    return timeUnit.convert(delta,TimeUnit.MILLISECONDS)
+    Math.abs((delta * date_unit).toInt)
   }
   
   def generate_feature(loc_train: String, loc_test: String, loc_transactions: String, loc_out_train: String, loc_out_test: String) = {
