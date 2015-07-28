01645fe6b75dfcc45e88128c633805b7b80070cb
Tianxiang(Ivan) Liu <ivan.liuyanfeng@gmail.com>
2015-07-28 11:51:17 +1000

    Date Difference

--- src/main/scala/utilClasses/utility.scala
+++ src/main/scala/utilClasses/utility.scala
@@ -4,11 +4,14 @@ package utilClasses
  * @author ivanliu
  */
 class utility {
-  def reduce_data(loc_offers: String, loc_transactions: String, loc_reduced: String) = {
-  }
   
-  def diff_days(s1: String, s2: String) = {
-    
+  val date_format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+  
+  def diff_days(s1: String, s2: String, TimeUnit timeUnit) = {
+    val date1 = date_format.parse(s1)
+    val date2 = date_format.parse(s2)
+    val delta: Long = date2.getTime() - date1.getTime()
+    return timeUnit.convert(delta,TimeUnit.MILLISECONDS)
   }
   
   def generate_feature(loc_train: String, loc_test: String, loc_transactions: String, loc_out_train: String, loc_out_test: String) = {
