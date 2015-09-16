f47ba3a990d18258758bd24525a6e372a95e818e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-09-16 20:50:12 +1000

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Spark_Hackathon
-Building a Streaming Machine Learning Algorithm for the Spark Hackathon hosted by Servian Ltd Pty
+Building a Streaming Machine Learning Algorithm for the Spark Hackathon hosted by Servian Ltd Pty <br>
+
+
+spark/bin/spark-submit --class "StreamingMachineLearning_Main" --master local[4] target/scala-2.11/spark_hackathon_2.11-1.0.jar ../data/trainHistory ../data/testHistory 3600 22
--- src/main/scala/StreamingMachineLearning_Main.scala
+++ src/main/scala/StreamingMachineLearning_Main.scala
@@ -15,18 +15,18 @@ object StreamingMachineLearning_Main {
   def main(args: Array[String]) {
 
     
-//    if (args.length != 4) {
-//      System.err.println(
-//        "Usage: StreamingLogisticRegression <trainingDir> <testDir> <BatchDuration> <trainHour>")
-//      System.exit(1)
-//    }
+    if (args.length != 4) {
+      System.err.println(
+        "Usage: StreamingLogisticRegression <trainingDir> <testDir> <BatchDuration> <trainHour>")
+      System.exit(1)
+    }
     
     
     /* 1. Setup Parameters */
     val conf = new SparkConf().setMaster("local").setAppName("StreamingLogisticRegression")
     conf.set("spark.driver.allowMultipleContexts","true")
-     val ssc = new StreamingContext(conf, Seconds(300))
-//    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
+//     val ssc = new StreamingContext(conf, Seconds(300))
+    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
 
     val now = new Date   
     val dateFormatter = new SimpleDateFormat("y-M-d")
@@ -43,12 +43,12 @@ object StreamingMachineLearning_Main {
     /* 2. Start to training data */
     val offer_path = "../data/offers"
     val transaction_path = "../data/transactions"
-//    val train_path = args(0).toString
-//    val test_path = args(1).toString
-//    val tHour = args(3).toInt
-    val train_path = "../data/trainHistory"
-    val test_path = "../data/testHistory"
-    val tHour = 21
+    val train_path = args(0).toString
+    val test_path = args(1).toString
+    val tHour = args(3).toInt
+//    val train_path = "../data/trainHistory"
+//    val test_path = "../data/testHistory"
+//    val tHour = 21
 
     
     /* 2.1 Training or Predicting */
